4d341781c0c6225695dc9d8d755649655a55f18c
codertcm <codertcm@gmail.com>
2019-09-14 07:57:58 +0800

    update

--- 字符串按特定字符拆分.cpp
+++ 字符串按特定字符拆分.cpp
@@ -3,24 +3,30 @@
 #include "bits/stdc++.h"
 using namespace std;
 
-int main()
-{
-    string s1;
-    getline(cin, s1);
-    vector<string> res;
+void split(string &s1, vector<string> &res, char c){
+    res.clear();
     while (!s1.empty())
     {
-        if (s1.find(" ") == string::npos)
+        if (s1.find(c) == string::npos)
         {
             res.push_back(s1);
             s1.clear();
             break;
         }
-		int id = sample.find(" ");
-		string temp = sample.substr(0, id);
-		v.push_back(temp);
-		sample.earse(0, id+1);
+		int id = s1.find(c);
+		string temp = s1.substr(0, id);
+		res.push_back(temp);
+		s1.erase(0, id+1);
     }
+}
+
+
+int main()
+{
+    string s1;
+    getline(cin, s1);
+    vector<string> res;
+    split(s1, res, ':');
     for (auto &a : res)
     {
         cout << a << endl;
